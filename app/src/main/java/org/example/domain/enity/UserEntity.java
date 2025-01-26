df8dafeea9ea1e46f879ad5d952784d93fc4cd0e
Abhishek Shrestha <meabhi224@gmail.com>
2025-01-26 11:09:10 +0545

    created User and UserRole Entity

---
+++ app/src/main/java/org/example/domain/enity/UserEntity.java
@@ -0,0 +1,36 @@
+package org.example.domain.enity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(table = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,  updatable = false)
+    private Long id;
+
+    @Column(nullable = false,  updatable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<UserRole> roles;
+
+}
---
+++ app/src/main/java/org/example/domain/enity/UserRole.java
@@ -0,0 +1,22 @@
+package org.example.domain.enity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "roles")
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, updatable = false, unique = true)
+    private Long userId;
+}
