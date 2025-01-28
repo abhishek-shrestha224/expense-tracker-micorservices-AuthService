ce7b2d7535bb86d6c53dfc3e42bd08f4cff322d7
Abhihsek Shrestha <meabhi224@gmail.com>
2025-01-28 12:06:49 +0545

    created a AuthController

--- app/src/main/java/authservice/controller/AuthController.java
+++ app/src/main/java/authservice/controller/AuthController.java
@@ -1,4 +1,31 @@
 package authservice.controller;
 
+import authservice.domain.dto.UserCreateDto;
+import authservice.service.JwtService;
+import authservice.service.RefreshTokenService;
+import authservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth/v1")
 public class AuthController {
+
+    private final JwtService jwtService;
+
+    private final RefreshTokenService refreshTokenService;
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(final JwtService jwtService, final RefreshTokenService refreshTokenService, final UserService userService) {
+        this.jwtService = jwtService;
+        this.refreshTokenService = refreshTokenService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public String SignUp(@RequestBody UserCreateDto createData) {
+        return null;
+    }
 }
